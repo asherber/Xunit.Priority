63a935bcc14fa6a48981ddfa30265bb7f3a68d34
Aaron Sherber <aaron@sherber.com>
2019-11-05 15:07:14 -0500

    Include XML documentation

--- Xunit.Priority/DefaultPriorityAttribute.cs
+++ Xunit.Priority/DefaultPriorityAttribute.cs
@@ -22,6 +22,10 @@ using System.Threading.Tasks;
 
 namespace Xunit.Priority
 {
+    /// <summary>
+    /// Indicates the priority value which will be assigned
+    /// to tests in this class which don't have a <see cref="PriorityAttribute"/>.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class DefaultPriorityAttribute: Attribute
     {
--- Xunit.Priority/PriorityAttribute.cs
+++ Xunit.Priority/PriorityAttribute.cs
@@ -19,8 +19,12 @@ using System;
 namespace Xunit.Priority
 {
     /// <summary>
-    /// Indicates relative priority of tests for execution. Tests with the same
-    /// priority are run in alphabetical order. Tests with no priority are run last.
+    /// <para>Indicates relative priority of tests for execution. Tests with the same
+    /// priority are run in alphabetical order. </para>
+    /// 
+    /// <para>Tests with no priority attribute
+    /// are assigned a priority of <see cref="int.MaxValue"/>,
+    /// unless the class has a <see cref="DefaultPriorityAttribute"/>.</para>
     /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
     public class PriorityAttribute : Attribute
